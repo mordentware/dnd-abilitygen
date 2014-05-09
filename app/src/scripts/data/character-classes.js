5dd1182662d005d55451db4615d38ab6b1989b99
Matt <matt@mordentware.com>
2014-05-09 17:15:30 +0000

    Adding race and class data.

--- app/src/scripts/data/character-classes.js
+++ app/src/scripts/data/character-classes.js
@@ -8,23 +8,227 @@ define([
 	 */
 	var characterClasses = {
 		'barbarian': new CharacterClass('Barbarian', 'simple', {
-			'rage': new CharacterClassVariant('Rage', {
+			'rage': new CharacterClassVariant('Standard', {
 				'str': 0,
 				'dex': 0,
+				'con': 1,
+				'int': 2,
+				'wis': 2,
+				'cha': 2
+			})
+		}),
+		'bard': new CharacterClass('Bard', 'moderate', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 2,
+				'dex': 0,
+				'con': 2,
+				'int': 0,
+				'wis': 1,
+				'cha': 1
+			})
+		}),
+		'cleric': new CharacterClass('Cleric', 'complex', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 1,
+				'dex': 2,
+				'con': 2,
+				'int': 2,
+				'wis': 0,
+				'cha': 1
+			}),
+			'mighty': new CharacterClassVariant('Mighty', {
+				'str': 0,
+				'dex': 2,
+				'con': 1,
+				'int': 2,
+				'wis': 0,
+				'cha': 1
+			}),
+			'nimble': new CharacterClassVariant('Nimble', {
+				'str': 2,
+				'dex': 0,
+				'con': 1,
+				'int': 2,
+				'wis': 0,
+				'cha': 1
+			})
+		}),
+		'druid': new CharacterClass('Druid', 'complex', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 2,
+				'dex': 1,
+				'con': 2,
+				'int': 2,
+				'wis': 0,
+				'cha': 1
+			})
+		}),
+		'fighter': new CharacterClass('Fighter', 'moderate', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 0,
+				'dex': 1,
+				'con': 1,
+				'int': 2,
+				'wis': 2,
+				'cha': 3
+			}),
+			'durable': new CharacterClassVariant('Durable', {
+				'str': 0,
+				'dex': 2,
+				'con': 1,
+				'int': 3,
+				'wis': 2,
+				'cha': 3
+			}),
+			'nimble': new CharacterClassVariant('Nimble', {
+				'str': 1,
+				'dex': 0,
+				'con': 1,
+				'int': 2,
+				'wis': 3,
+				'cha': 3
+			}),
+		}),
+		'monk': new CharacterClass('Monk', 'moderate', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 2,
+				'dex': 1,
+				'con': 1,
+				'int': 2,
+				'wis': 0,
+				'cha': 2
+			})
+		}),
+		'paladin': new CharacterClass('Paladin', 'moderate', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 1,
+				'dex': 0,
+				'con': 1,
+				'int': 2,
+				'wis': 3,
+				'cha': 3
+			})
+		}),
+		'ranger': new CharacterClass('Ranger', 'moderate', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 1,
+				'dex': 0,
+				'con': 1,
+				'int': 2,
+				'wis': 3,
+				'cha': 3
+			}),
+			'cunning': new CharacterClassVariant('Cunning', {
+				'str': 1,
+				'dex': 0,
+				'con': 1,
+				'int': 0,
+				'wis': 2,
+				'cha': 2
+			})
+		}),
+		'rogue': new CharacterClass('Rogue', 'simple', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 2,
+				'dex': 0,
+				'con': 1,
+				'int': 1,
+				'wis': 2,
+				'cha': 2
+			})
+		}),
+		'sorcerer': new CharacterClass('Sorcerer', 'simple', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 3,
+				'dex': 1,
+				'con': 2,
+				'int': 0,
+				'wis': 2,
+				'cha': 1
+			})
+		}),
+		'wizard': new CharacterClass('Wizard', 'complex', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 2,
+				'dex': 1,
+				'con': 2,
+				'int': 0,
+				'wis': 1,
+				'cha': 2
+			})
+		}),
+		'adept': new CharacterClass('Adept', 'complex', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 2,
+				'dex': 2,
+				'con': 2,
+				'int': 1,
+				'wis': 0,
+				'cha': 1
+			})
+		}),
+		'aristocrat': new CharacterClass('Aristocrat', 'complex', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 0,
+				'dex': 1,
+				'con': 1,
+				'int': 0,
+				'wis': 1,
+				'cha': 0
+			})
+		}),
+		'commoner': new CharacterClass('Commoner', 'simple', {
+			'manual': new CharacterClassVariant('Manual', {
+				'str': 0,
+				'dex': 1,
 				'con': 0,
+				'int': 2,
+				'wis': 2,
+				'cha': 2
+			}),
+			'administrative': new CharacterClassVariant('Administrative', {
+				'str': 1,
+				'dex': 1,
+				'con': 1,
 				'int': 0,
 				'wis': 0,
 				'cha': 0
 			})
 		}),
-		'bard': new CharacterClass('Bard', 'moderate', {
+		'expert': new CharacterClass('Expert', 'moderate', {
+			'standard': new CharacterClassVariant('Standard', {
+				'str': 2,
+				'dex': 1,
+				'con': 2,
+				'int': 0,
+				'wis': 1,
+				'cha': 0
+			})
+		}),
+		'warrior': new CharacterClass('Warrior', 'simple', {
 			'standard': new CharacterClassVariant('Standard', {
 				'str': 0,
+				'dex': 1,
+				'con': 1,
+				'int': 2,
+				'wis': 2,
+				'cha': 3
+			}),
+			'durable': new CharacterClassVariant('Durable', {
+				'str': 0,
+				'dex': 2,
+				'con': 1,
+				'int': 3,
+				'wis': 2,
+				'cha': 3
+			}),
+			'nimble': new CharacterClassVariant('Nimble', {
+				'str': 1,
 				'dex': 0,
-				'con': 0,
-				'int': 0,
-				'wis': 0,
-				'cha': 0
+				'con': 1,
+				'int': 2,
+				'wis': 3,
+				'cha': 3
 			})
 		})
 	};
--- app/src/scripts/data/character-races.js
+++ app/src/scripts/data/character-races.js
@@ -6,40 +6,6 @@ define([
 	 * TODO: documentation
 	 */
 	var characterRaces = {
-		'elf': new CharacterRace('Elf', {
-			'str': 0,
-			'dex': 2,
-			'con': -2,
-			'int': 0,
-			'wis': 0,
-			'cha': 0
-		}, {
-			// TODO: update
-			'male': {
-				'heightBase': 50,
-				'heightModifier': '1d10',
-				'weightBase': 100,
-				'weightMultiplier': '1d10'
-			},
-			'female': {
-				'heightBase': 50,
-				'heightModifier': '1d10',
-				'weightBase': 100,
-				'weightMultiplier': '1d10'
-			}
-		}, {
-			// TODO: update
-			'simple': '1d10',
-			'moderate': '2d10',
-			'complex': '3d10'
-		}, {
-			// TODO: update
-			'adult': 100,
-			'middleaged': 200,
-			'old': 300,
-			'venerable': 400,
-			'maximum': '1d100'
-		}),
 		'human': new CharacterRace('Human', {
 			'str': 0,
 			'dex': 0,
@@ -65,12 +31,197 @@ define([
 			'moderate': '1d6',
 			'complex': '2d6'
 		}, {
-			// TODO: update
 			'adult': 15,
-			'middleaged': 30,
-			'old': 50,
+			'middleaged': 35,
+			'old': 53,
 			'venerable': 70,
-			'maximum': '1d20'
+			'maximum': '2d20'
+		}),
+		'dwarf': new CharacterRace('Dwarf', {
+			'str': 0,
+			'dex': 0,
+			'con': 2,
+			'int': 0,
+			'wis': 0,
+			'cha': -2
+		}, {
+			'male': {
+				'heightBase': 45,
+				'heightModifier': '2d4',
+				'weightBase': 130,
+				'weightMultiplier': '2d6'
+			},
+			'female': {
+				'heightBase': 43,
+				'heightModifier': '2d4',
+				'weightBase': 100,
+				'weightModifier': '2d6'
+			}
+		}, {
+			'simple': '3d6',
+			'moderate': '5d6',
+			'complex': '7d6'
+		}, {
+			'adult': 40,
+			'middleaged': 125,
+			'old': 188,
+			'venerable': 250,
+			'maximum': '2d100'
+		}),
+		'elf': new CharacterRace('Elf', {
+			'str': 0,
+			'dex': 2,
+			'con': -2,
+			'int': 0,
+			'wis': 0,
+			'cha': 0
+		}, {
+			'male': {
+				'heightBase': 53,
+				'heightModifier': '2d6',
+				'weightBase': 85,
+				'weightMultiplier': '1d6'
+			},
+			'female': {
+				'heightBase': 53,
+				'heightModifier': '2d6',
+				'weightBase': 80,
+				'weightMultiplier': '1d6'
+			}
+		}, {
+			'simple': '4d6',
+			'moderate': '6d6',
+			'complex': '10d6'
+		}, {
+			'adult': 110,
+			'middleaged': 175,
+			'old': 263,
+			'venerable': 350,
+			'maximum': '4d100'
+		}),
+		'gnome': new CharacterRace('Gnome', {
+			'str': -2,
+			'dex': 0,
+			'con': 2,
+			'int': 0,
+			'wis': 0,
+			'cha': 0
+		}, {
+			'male': {
+				'heightBase': 36,
+				'heightModifier': '2d4',
+				'weightBase': 40,
+				'weightMultiplier': '1d1'
+			},
+			'female': {
+				'heightBase': 34,
+				'heightModifier': '2d4',
+				'weightBase': 35,
+				'weightMultiplier': '1d4'
+			}
+		}, {
+			'simple': '4d6',
+			'moderate': '6d6',
+			'complex': '9d6'
+		}, {
+			'adult': 40,
+			'middleaged': 100,
+			'old': 150,
+			'venerable': 200,
+			'maximum': '3d100'
+		}),
+		'halfling': new CharacterRace('Halfling', {
+			'str': -2,
+			'dex': 2,
+			'con': 0,
+			'int': 0,
+			'wis': 0,
+			'cha': 0
+		}, {
+			'male': {
+				'heightBase': 32,
+				'heightModifier': '2d4',
+				'weightBase': 30,
+				'weightMultiplier': '1d1'
+			},
+			'female': {
+				'heightBase': 30,
+				'heightModifier': '2d4',
+				'weightBase': 25,
+				'weightMultiplier': '1d1'
+			}
+		}, {
+			'simple': '2d4',
+			'moderate': '3d6',
+			'complex': '4d6'
+		}, {
+			'adult': 20,
+			'middleaged': 50,
+			'old': 75,
+			'venerable': 100,
+			'maximum': '5d20'
+		}),
+		'halfelf': new CharacterRace('Half-elf', {
+			'str': 0,
+			'dex': 0,
+			'con': 0,
+			'int': 0,
+			'wis': 0,
+			'cha': 0
+		}, {
+			'male': {
+				'heightBase': 55,
+				'heightModifier': '2d8',
+				'weightBase': 100,
+				'weightMultiplier': '2d4'
+			},
+			'female': {
+				'heightBase': 53,
+				'heightModifier': '2d8',
+				'weightBase': 80,
+				'weightMultiplier': '2d4'
+			}
+		}, {
+			'simple': '1d6',
+			'moderate': '2d6',
+			'complex': '3d6'
+		}, {
+			'adult': 20,
+			'middleaged': 62,
+			'old': 93,
+			'venerable': 125,
+			'maximum': '3d20'
+		}),
+		'halforc': new CharacterRace('Half-orc', {
+			'str': 2,
+			'dex': 0,
+			'con': 0,
+			'int': -2,
+			'wis': 0,
+			'cha': -2
+		}, {
+			'male': {
+				'heightBase': 58,
+				'heightModifier': '2d12',
+				'weightBase': 150,
+				'weightMultiplier': '2d6'
+			},
+			'female': {
+				'heightBase': 53,
+				'heightModifier': '2d12',
+				'weightBase': 110,
+				'weightMultiplier': '2d6'
+			}
+		}, {
+			'simple': '1d4',
+			'moderate': '1d6',
+			'complex': '2d6'
+		}, {
+			'adult': 14,
+			'middleaged': 30,
+			'old': 45,
+			'venerable': 60,
+			'maximum': '2d10'
 		})
 	};
 
